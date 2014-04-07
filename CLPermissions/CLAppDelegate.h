b9914c6aa45a89f7949e3bb999f14f2f9356d874
Rizwan Sattar <rsattar@gmail.com>
2014-04-07 15:12:23 -0700

    Initial Commit

---
+++ CLPermissions/CLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLAppDelegate.h
+//  CLPermissions
+//
+//  Created by Rizwan Sattar on 4/7/14.
+//  Copyright (c) 2014 Cluster Labs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CLPermissions/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CLPermissions
+//
+//  Created by Rizwan Sattar on 4/7/14.
+//  Copyright (c) 2014 Cluster Labs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLAppDelegate class]));
+    }
+}
